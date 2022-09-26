c9693d4ea4a61a10aa4df6cdca7bb0f9eb47bf22
Bhumik Kalola21 <bhumik.kalola@bacancy.com>
2022-09-26 17:40:19 +0530

    Validation done

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,521 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="login-root">
+    <div
+      class="box-root flex-flex flex-direction--column"
+      style="min-height: 100vh; flex-grow: 1"
+    >
+      <div class="loginbackground box-background--white padding-top--64">
+        <div class="loginbackground-gridContainer">
+          <div
+            class="box-root flex-flex"
+            style="grid-area: top / start / 8 / end"
+          >
+            <div
+              class="box-root"
+              style="
+                background-image: linear-gradient(
+                  white 0%,
+                  rgb(247, 250, 252) 33%
+                );
+                flex-grow: 1;
+              "
+            ></div>
+          </div>
+          <div class="box-root flex-flex" style="grid-area: 4 / 2 / auto / 5">
+            <div
+              class="box-root box-divider--light-all-2 animationLeftRight tans3s"
+              style="flex-grow: 1"
+            ></div>
+          </div>
+          <div
+            class="box-root flex-flex"
+            style="grid-area: 6 / start / auto / 2"
+          >
+            <div
+              class="box-root box-background--blue800"
+              style="flex-grow: 1"
+            ></div>
+          </div>
+          <div
+            class="box-root flex-flex"
+            style="grid-area: 7 / start / auto / 4"
+          >
+            <div
+              class="box-root box-background--blue animationLeftRight"
+              style="flex-grow: 1"
+            ></div>
+          </div>
+          <div class="box-root flex-flex" style="grid-area: 8 / 4 / auto / 6">
+            <div
+              class="box-root box-background--gray100 animationLeftRight tans3s"
+              style="flex-grow: 1"
+            ></div>
+          </div>
+          <div
+            class="box-root flex-flex"
+            style="grid-area: 2 / 15 / auto / end"
+          >
+            <div
+              class="box-root box-background--cyan200 animationRightLeft tans4s"
+              style="flex-grow: 1"
+            ></div>
+          </div>
+          <div
+            class="box-root flex-flex"
+            style="grid-area: 3 / 14 / auto / end"
+          >
+            <div
+              class="box-root box-background--blue animationRightLeft"
+              style="flex-grow: 1"
+            ></div>
+          </div>
+          <div class="box-root flex-flex" style="grid-area: 4 / 17 / auto / 20">
+            <div
+              class="box-root box-background--gray100 animationRightLeft tans4s"
+              style="flex-grow: 1"
+            ></div>
+          </div>
+          <div class="box-root flex-flex" style="grid-area: 5 / 14 / auto / 17">
+            <div
+              class="box-root box-divider--light-all-2 animationRightLeft tans3s"
+              style="flex-grow: 1"
+            ></div>
+          </div>
+        </div>
+      </div>
+      <div
+        class="box-root padding-top--24 flex-flex flex-direction--column"
+        style="flex-grow: 1; z-index: 9"
+      >
+        <div
+          class="box-root padding-top--48 padding-bottom--24 flex-flex flex-justifyContent--center"
+        >
+          <h1>
+            <a href="http://blog.stackfindover.com/" rel="dofollow"
+              >ValidationDemo</a
+            >
+          </h1>
+        </div>
+        <div class="formbg-outer">
+          <div class="formbg">
+            <div class="formbg-inner padding-horizontal--48">
+              <span class="padding-bottom--15">Register your self</span>
+              <form @submit.prevent id="stripe-login">
+                <div class="field padding-bottom--24">
+                  <label for="name">Name</label>
+                  <input
+                    type="name"
+                    name="name"
+                    v-model="information.name"
+                    @blur="$v.information.name.$touch()"
+                  />
+                  <template v-if="$v.information.name.$error">
+                    <p
+                      class="errorMessage"
+                      v-if="!$v.information.name.required"
+                    >
+                      Name is required*
+                    </p>
+                    <p
+                      class="errorMessage"
+                      v-if="!$v.information.name.minLength"
+                    >
+                      Name is must 4 characters.
+                    </p>
+                  </template>
+                </div>
+                <div class="field padding-bottom--24">
+                  <label for="email">Email</label>
+                  <input
+                    type="email"
+                    name="email"
+                    v-model="information.email"
+                    @blur="$v.information.email.$touch()"
+                  />
+                  <template v-if="$v.information.email.$error">
+                    <p
+                      class="errorMessage"
+                      v-if="!$v.information.email.required"
+                    >
+                      email is required*
+                    </p>
+                    <p class="errorMessage" v-if="!$v.information.email.email">
+                      email is invalid*
+                    </p>
+                  </template>
+                </div>
+                <div class="field padding-bottom--24">
+                  <label for="password">Password</label>
+                  <input
+                    type="password"
+                    name="password"
+                    v-model="information.password"
+                    @blur="$v.information.password.$touch()"
+                  />
+                  <template v-if="$v.information.password.$error">
+                    <p
+                      class="errorMessage"
+                      v-if="!$v.information.password.required " 
+                    >
+                      password is required*
+                    </p>
+                    <p
+                      class="errorMessage"
+                      v-if="!$v.information.password.minLength"
+                    >
+                      Password must be 8 characters.
+                    </p>
+                    <p
+                      class="errorMessage"
+                      v-if="!$v.information.password.containsNumber.required"
+                    >
+                      password is required containsNumber*
+                    </p>
+                    <p
+                      class="errorMessage"
+                      v-if="!$v.information.password.containsUppercase.required">
+                      password is required containsUppercase*
+                    </p>
+                    <p
+                      class="errorMessage"
+                      v-if="!$v.information.password.containsLowercase.required"
+                    >
+                      password is required containsLowercase*
+                    </p>
+                    <p
+                      class="errorMessage"
+                      v-if="!$v.information.password.containsSpecial.required"
+                    >
+                      password is required containsSpecial characters*
+                    </p>
+                  </template>
+                </div>
+                <div class="field padding-bottom--24">
+                  <div class="grid--50-50">
+                    <label for="confirmPassword">ConfirmPassword</label>
+                  </div>
+                  <input
+                    type="confirmPassword"
+                    name="confirmPassword"
+                    v-model="information.confirmPassword"
+                    @blur="$v.information.confirmPassword.$touch()"
+                  />
+                  <template v-if="$v.information.confirmPassword.$error">
+                    <p
+                      class="errorMessage"
+                      v-if="!$v.information.confirmPassword.required"
+                    >
+                      confirmPassword is required*
+                    </p>
+                    <p
+                      class="errorMessage"
+                      v-if="!$v.information.confirmPassword.minLength"
+                    >
+                      Password must be 8 characters.
+                    </p>
+                    <p
+                      class="errorMessage"
+                      v-if="
+                        $v.information.confirmPassword !=
+                        $v.information.password
+                      "
+                    >
+                      confirm password not match with password.
+                    </p>
+                  </template>
+                </div>
+                <div class="field padding-bottom--24">
+                  <button
+                    type="button"
+                    class="btn btn-primary"
+                    :disabled="$v.$invalid"
+                    @click="submitData"
+                  >
+                    continue
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
-
 <script>
+import { required, email, minLength, sameAs } from "vuelidate/lib/validators";
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+  data() {
+    return {
+      information: {
+        name: "",
+        email: "",
+        password: "",
+        confirmPassword: "",
+      },
+    };
+  },
+  validations: {
+    information: {
+      name: {
+        required,
+        minLength: minLength(4),
+      },
+      email: {
+        required,
+        email,
+      },
+      password: {
+        required,
+        minLength: minLength(8),
+        containsUppercase: function (value) {
+          return /[A-Z]/.test(value);
+        },
+        containsLowercase: function (value) {
+          return /[a-z]/.test(value);
+        },
+        containsNumber: function (value) {
+          return /[0-9]/.test(value);
+        },
+        containsSpecial: function (value) {
+          return /[#?!@$%^&*-]/.test(value);
+        },
+      },
+      confirmPassword: {
+        required,
+        minlength: minLength(8),
+        sameAs: sameAs(function () {
+          return this.information.password;
+        }),
+      },
+    },
+  },
+  methods: {
+    submitData() {
+      this.$v.$touch();
+      if (!this.$v.$invalid) {
+        console.log("Form submiited successully");
+        console.log(this.information);
+      }
+    },
+  },
+};
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
+* {
   padding: 0;
+  margin: 0;
+  color: #1a1f36;
+  box-sizing: border-box;
+  word-wrap: break-word;
+  font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
+    Helvetica Neue, Ubuntu, sans-serif;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+body {
+  min-height: 100%;
+  background-color: #ffffff;
+}
+h1 {
+  letter-spacing: -1px;
 }
 a {
-  color: #42b983;
+  color: #5469d4;
+  text-decoration: unset;
+}
+.login-root {
+  background: #fff;
+  display: flex;
+  width: 100%;
+  min-height: 100vh;
+  overflow: hidden;
+}
+.loginbackground {
+  min-height: 692px;
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  top: 0;
+  z-index: 0;
+  overflow: hidden;
+}
+.flex-flex {
+  display: flex;
+}
+.align-center {
+  align-items: center;
+}
+.center-center {
+  align-items: center;
+  justify-content: center;
+}
+.box-root {
+  box-sizing: border-box;
+}
+.flex-direction--column {
+  -ms-flex-direction: column;
+  flex-direction: column;
+}
+.loginbackground-gridContainer {
+  display: -ms-grid;
+  display: grid;
+  -ms-grid-columns: [start] 1fr [left-gutter] (86.6px) [16] [left-gutter] 1fr
+    [end];
+  grid-template-columns: [start] 1fr [left-gutter] repeat(16, 86.6px) [left-gutter] 1fr [end];
+  -ms-grid-rows: [top] 1fr [top-gutter] (64px) [8] [bottom-gutter] 1fr [bottom];
+  grid-template-rows: [top] 1fr [top-gutter] repeat(8, 64px) [bottom-gutter] 1fr [bottom];
+  justify-content: center;
+  margin: 0 -2%;
+  transform: rotate(-12deg) skew(-12deg);
+}
+.box-divider--light-all-2 {
+  box-shadow: inset 0 0 0 2px #e3e8ee;
+}
+.box-background--blue {
+  background-color: #5469d4;
+}
+.box-background--white {
+  background-color: #ffffff;
+}
+.box-background--blue800 {
+  background-color: #212d63;
+}
+.box-background--gray100 {
+  background-color: #e3e8ee;
+}
+.box-background--cyan200 {
+  background-color: #7fd3ed;
+}
+.padding-top--64 {
+  padding-top: 64px;
+}
+.padding-top--24 {
+  padding-top: 24px;
+}
+.padding-top--48 {
+  padding-top: 48px;
+}
+.padding-bottom--24 {
+  padding-bottom: 24px;
+}
+.padding-horizontal--48 {
+  padding: 48px;
+}
+.padding-bottom--15 {
+  padding-bottom: 15px;
+}
+
+.flex-justifyContent--center {
+  -ms-flex-pack: center;
+  justify-content: center;
+}
+
+.formbg {
+  margin: 0px auto;
+  width: 100%;
+  max-width: 448px;
+  background: white;
+  border-radius: 4px;
+  box-shadow: rgba(60, 66, 87, 0.12) 0px 7px 14px 0px,
+    rgba(0, 0, 0, 0.12) 0px 3px 6px 0px;
+}
+span {
+  display: block;
+  font-size: 20px;
+  line-height: 28px;
+  color: #1a1f36;
+}
+label {
+  margin-bottom: 10px;
+}
+.reset-pass a,
+label {
+  font-size: 14px;
+  font-weight: 600;
+  display: block;
+}
+.reset-pass > a {
+  text-align: right;
+  margin-bottom: 10px;
+}
+.grid--50-50 {
+  display: grid;
+  grid-template-columns: 50% 50%;
+  align-items: center;
+}
+
+.field input {
+  font-size: 16px;
+  line-height: 28px;
+  padding: 8px 16px;
+  width: 100%;
+  min-height: 44px;
+  border: unset;
+  border-radius: 4px;
+  outline-color: rgb(84 105 212 / 0.5);
+  background-color: rgb(255, 255, 255);
+  box-shadow: rgba(0, 0, 0, 0) 0px 0px 0px 0px, rgba(0, 0, 0, 0) 0px 0px 0px 0px,
+    rgba(0, 0, 0, 0) 0px 0px 0px 0px, rgba(60, 66, 87, 0.16) 0px 0px 0px 1px,
+    rgba(0, 0, 0, 0) 0px 0px 0px 0px, rgba(0, 0, 0, 0) 0px 0px 0px 0px,
+    rgba(0, 0, 0, 0) 0px 0px 0px 0px;
+}
+
+input[type="submit"] {
+  background-color: rgb(84, 105, 212);
+  box-shadow: rgba(0, 0, 0, 0) 0px 0px 0px 0px, rgba(0, 0, 0, 0) 0px 0px 0px 0px,
+    rgba(0, 0, 0, 0.12) 0px 1px 1px 0px, rgb(84, 105, 212) 0px 0px 0px 1px,
+    rgba(0, 0, 0, 0) 0px 0px 0px 0px, rgba(0, 0, 0, 0) 0px 0px 0px 0px,
+    rgba(60, 66, 87, 0.08) 0px 2px 5px 0px;
+  color: #fff;
+  font-weight: 600;
+  cursor: pointer;
+}
+a.ssolink {
+  display: block;
+  text-align: center;
+  font-weight: 600;
+}
+
+.animationRightLeft {
+  animation: animationRightLeft 2s ease-in-out infinite;
+}
+.animationLeftRight {
+  animation: animationLeftRight 2s ease-in-out infinite;
+}
+.tans3s {
+  animation: animationLeftRight 3s ease-in-out infinite;
+}
+.tans4s {
+  animation: animationLeftRight 4s ease-in-out infinite;
+}
+
+button {
+  width: 22rem;
+  margin: auto;
+  padding: 7px;
+}
+
+@keyframes animationLeftRight {
+  0% {
+    transform: translateX(0px);
+  }
+  50% {
+    transform: translateX(1000px);
+  }
+  100% {
+    transform: translateX(0px);
+  }
+}
+
+@keyframes animationRightLeft {
+  0% {
+    transform: translateX(0px);
+  }
+  50% {
+    transform: translateX(-1000px);
+  }
+  100% {
+    transform: translateX(0px);
+  }
 }
 </style>
